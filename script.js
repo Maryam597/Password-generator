651894fb061903482325a1436aadf502f104ead5
Maryam597 <maryam.dri@hotmail.fr>
2023-06-23 13:52:22 +0200

    Password Generator

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+const c = 'AZERTYUIOPMLKJHGFDSQNBVCXWazertyuiopmlkjhgfdsqwxcvbn0123456789*/&é(-è_çà@)]=$ù!:;,';
+
+const longueur = 20
+
 function generatePassword(event){
     event.preventDefault()
+    let input = document.querySelector('#password')
+    let motDePasse = '';
+    for(let i = 0; i < longueur; i++) { // boucle sur le nombre de caractères donnés. Chaque itération va être un caratère du mdp 
+        const indexC = Math.floor(Math.random() * c.length);
+        motDePasse += c[indexC];
+
+
+    }
+
+input.value = motDePasse
+
 }
 
-
-const c = 'AZERTYUIOPMLKJHGFDSQNBVCXWazertyuiopmlkjhgfdsqwxcvbn0123456789'
